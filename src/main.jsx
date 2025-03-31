d97e660ea1af3beeab979d96229ad5df3c732f73
facordoba02 <facordoba02@gmail.com>
2025-03-31 18:26:33 -0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
+import { HashRouter } from "react-router-dom";
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import '../styles/global.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <HashRouter>
     <App />
-  </StrictMode>,
+  </HashRouter>,
 )
